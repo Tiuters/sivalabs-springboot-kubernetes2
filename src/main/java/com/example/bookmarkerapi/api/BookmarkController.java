b1a18c28b9be10e8fd2a2f0b8a7e4d8fab1675cc
Tiuters <ands458@gmail.com>
2023-10-16 22:02:36 +0300

    part 3. BookmarksDTO  info about pages

--- src/main/java/com/example/bookmarkerapi/api/BookmarkController.java
+++ src/main/java/com/example/bookmarkerapi/api/BookmarkController.java
@@ -2,6 +2,7 @@ package com.example.bookmarkerapi.api;
 
 import com.example.bookmarkerapi.domain.Bookmark;
 import com.example.bookmarkerapi.domain.BookmarkService;
+import com.example.bookmarkerapi.domain.BookmarksDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,7 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @GetMapping
-    public List<Bookmark> getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page) {
+    public BookmarksDTO getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page) {
         return bookmarkService.getBookmarks(page);
     }
 
--- src/main/java/com/example/bookmarkerapi/domain/BookmarkService.java
+++ src/main/java/com/example/bookmarkerapi/domain/BookmarkService.java
@@ -16,10 +16,10 @@ public class BookmarkService {
     private final BookmarkRepository bookmarkRepository;
 
     @Transactional(readOnly = true)
-    public List<Bookmark> getBookmarks(Integer page) {
+    public BookmarksDTO getBookmarks(Integer page) {
         int pageNo = page < 1 ? 0 : (page - 1);
         PageRequest pageRequest = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "createdAt");
-        return bookmarkRepository.findAll(pageRequest).getContent();
+        return new BookmarksDTO(bookmarkRepository.findAll(pageRequest));
     }
 
 }
