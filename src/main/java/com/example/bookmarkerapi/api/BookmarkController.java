7f1c39a9c937d4b29a0a42e55321aee26250cc17
Tiuters <ands458@gmail.com>
2023-10-16 21:52:16 +0300

    part 3. paging

--- src/main/java/com/example/bookmarkerapi/api/BookmarkController.java
+++ src/main/java/com/example/bookmarkerapi/api/BookmarkController.java
@@ -5,6 +5,7 @@ import com.example.bookmarkerapi.domain.BookmarkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,8 +18,8 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @GetMapping
-    public List<Bookmark> getBookmarks() {
-        return bookmarkService.getBookmarks();
+    public List<Bookmark> getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page) {
+        return bookmarkService.getBookmarks(page);
     }
 
 }
--- src/main/java/com/example/bookmarkerapi/domain/BookmarkService.java
+++ src/main/java/com/example/bookmarkerapi/domain/BookmarkService.java
@@ -1,21 +1,25 @@
 package com.example.bookmarkerapi.domain;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
-//@Transactional
+@Transactional
 @RequiredArgsConstructor
 public class BookmarkService {
 
     private final BookmarkRepository bookmarkRepository;
 
-//    @Transactional(readOnly = true)
-    public List<Bookmark> getBookmarks() {
-        return bookmarkRepository.findAll();
+    @Transactional(readOnly = true)
+    public List<Bookmark> getBookmarks(Integer page) {
+        int pageNo = page < 1 ? 0 : (page - 1);
+        PageRequest pageRequest = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "createdAt");
+        return bookmarkRepository.findAll(pageRequest).getContent();
     }
 
 }
