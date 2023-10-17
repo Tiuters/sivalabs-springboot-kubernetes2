699295762a942d2837af9aa3a781310dea307f7d
Tiuters <ands458@gmail.com>
2023-10-17 13:14:46 +0300

    part 7. ---

--- src/main/java/com/example/bookmarkerapi/api/BookmarkController.java
+++ src/main/java/com/example/bookmarkerapi/api/BookmarkController.java
@@ -1,6 +1,5 @@
 package com.example.bookmarkerapi.api;
 
-import com.example.bookmarkerapi.domain.Bookmark;
 import com.example.bookmarkerapi.domain.BookmarkService;
 import com.example.bookmarkerapi.domain.BookmarksDTO;
 import lombok.RequiredArgsConstructor;
@@ -8,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/bookmarks")
